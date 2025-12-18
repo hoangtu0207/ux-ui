f1663d272ea917f32665b05ec33ca96a4b51215f
hoangtu0207 <hoanganhtu147@gmail.com>
2025-12-18 09:25:48 +0700

    Trang chu

--- src/Trangchu.jsx
+++ src/Trangchu.jsx
@@ -1,3 +1,32 @@
 export default function Trangchu() {
-	return <h1>Trang chu</h1>;
+	return (
+		<div>
+			<div className="bg-blue-100 flex flex-col w-full justify-center items-center h-60">
+				<span className="font-bold text-xl py-5">
+					Chao mung ban den voi LingGo!!!
+				</span>
+				<span className="font-semibold text-lg w-3/4 pb-10 flex flex-col justify-center items-center">
+					Chuc ban co trai nghiem hoc tap hieu qua voi LingGo.
+					<span className="">Hay bat dau hoc tap ngay hom nay</span>
+				</span>
+			</div>
+
+			<div>
+				<div className="flex justify-center flex-col items-center">
+					<div className="font-lg font-semibold py-5">Khoa hoc Online noi bat</div>
+					<div className="grid grid-cols-3 gap-10">
+						<div className="bg-gray-200 w-40 h-40 justify-center items-center text-center py-auto flex">
+							Khoa hoc 1
+						</div>
+						<div className="bg-gray-200 w-40 h-40 justify-center items-center text-center py-auto flex">
+							Khoa hoc 1
+						</div>
+						<div className="bg-gray-200 w-40 h-40 justify-center items-center text-center py-auto flex">
+							Khoa hoc 1
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
 }
