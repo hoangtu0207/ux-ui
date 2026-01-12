3e5e57604682a8ac393162e7359bd859f5d25259
hoangtu0207 <hoanganhtu147@gmail.com>
2026-01-13 05:10:11 +0700

    lien he va gioi thieu

--- src/App.jsx
+++ src/App.jsx
@@ -2,22 +2,17 @@ import { Routes, Route } from "react-router-dom";
 import Home from "./Home";
 import Dangnhap from "./Auth/Dangnhap";
 import Dangky from "./Auth/Dangky";
+import Gioithieu from "./Gioithieu";
+import Lienhe from "./Lienhe";
 
 function App() {
 	return (
 		<Routes>
-			<Route
-				path="/"
-				element={<Home />}
-			/>
-			<Route
-				path="/dangnhap"
-				element={<Dangnhap />}
-			/>
-			<Route
-				path="/dangky"
-				element={<Dangky />}
-			/>
+			<Route path="/" element={<Home />} />
+			<Route path="/dangnhap" element={<Dangnhap />} />
+			<Route path="/dangky" element={<Dangky />} />
+			<Route path="/gioithieu" element={<Gioithieu />} />
+			<Route path="/lienhe" element={<Lienhe />} />
 		</Routes>
 	);
 }
